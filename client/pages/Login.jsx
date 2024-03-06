d6f6a123fc5af53e9ab2ac74d8b964790086700f
sahil gupta <8278087870sahil@gmail>
2024-03-06 15:12:40 +0530

    feat: install tailwind

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,7 +1,43 @@
-
+import { useState } from "react";
 
 const login = () => {
-  return <div>login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  function changeHandler(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+  function submitHandler(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="p-3 max-w-lg mx-auto mt-40">
+      <h1 className="text-3xl my-7 text-center font-semibold">Log-In</h1>
+      <form onSubmit={submitHandler} className="flex flex-col gap-3">
+        <input
+          className="border p-3 rounded-lg focus:outline-none"
+          type="email"
+          placeholder="email"
+          name="email"
+          value={formData.eamil}
+          onChange={changeHandler}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={formData.password}
+          onChange={changeHandler}
+        />
+        <button>LOGIN</button>
+      </form>
+    </div>
+  );
 };
 
 export default login;
