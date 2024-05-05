3e2afdd9a3f69e955839823f8247b79986dfed39
sahil gupta <8278087870sahil@gmail>
2024-05-05 19:54:26 +0530

    Docs : add pre installation

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const userRouter = require("./routes/userroute");
 const adminRouter = require("./routes/adminroute");
 const doctorRouter = require("./routes/doctorroute");
+const path = require("path");
+const _dirname = path.resolve();
 app.use(express.json());
 
 require("./config/userdata").connect();
@@ -12,6 +14,11 @@ require("./config/userdata").connect();
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`sever listening on  ${port}`);
